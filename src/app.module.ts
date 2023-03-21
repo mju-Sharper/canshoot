2116336467fb2847e393d3aa801b0c2cffe4afba
ChoiWonYu <phillip07290@naver.com>
2023-03-21 16:55:29 +0900

    refactor: typeormconfig 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import authConfig from './config/auth.config';
-import typeormConfig from './config/typeorm.config';
+import { typeORMConfig } from './config/typeorm.config';
 import { validationSchema } from './config/validationSchema';
 
 const businessModules = [];
@@ -13,23 +12,9 @@ const businessModules = [];
 const libModules = [
   ConfigModule.forRoot({
     isGlobal: true,
-    load: [typeormConfig, authConfig],
     validationSchema,
   }),
-  TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [typeormConfig.KEY],
-    useFactory: (config: ConfigType<typeof typeormConfig>) => ({
-      type: 'mysql',
-      host: config.host,
-      port: +config.port,
-      username: config.username,
-      password: config.password,
-      database: config.database,
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
-  }),
+  TypeOrmModule.forRoot(typeORMConfig),
 ];
 
 @Module({
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,9 +1,12 @@
-import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export default registerAs('db', () => ({
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-}));
+  synchronize: true,
+  autoLoadEntities: true,
+};
