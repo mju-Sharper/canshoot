a3ce44ca164f7c8e428c44737146dea488354b23
ChoiWonYu <phillip07290@naver.com>
2023-03-29 12:51:14 +0900

    feat: default port 변경

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,7 +7,7 @@ export class DatabaseConfiguration implements TypeOrmOptionsFactory {
     return {
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
