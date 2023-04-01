defd0b2ab96506badff42ffb011bb8e261cdf464
ChoiWonYu <phillip07290@naver.com>
2023-04-01 23:44:09 +0900

    feat: 유효성 검사

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,9 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches, Min } from 'class-validator';
 
 export class SignInDto {
   @IsString()
+  @Min(4)
   userId: string;
 
   @IsString()
+  @Matches(/^[A-Za-z0-9]{6,}$/, {
+    message: '비밀번호는 최소 6자 이상의 문자와 숫자의 조합이어야 합니다.',
+  })
   password: string;
 }
--- src/auth/dto/signUp.dto.ts
+++ src/auth/dto/signUp.dto.ts
@@ -1,18 +1,24 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString, Matches, Min } from 'class-validator';
 
 export class SignUpDto {
   @IsString()
   name: string;
 
   @IsString()
+  @Matches(/\d{3}\d{4}\d{4}/)
   phone: string;
 
   @IsString()
+  @IsEmail()
   email: string;
 
   @IsString()
+  @Min(4)
   userId: string;
 
   @IsString()
+  @Matches(/^[A-Za-z0-9]{6,}$/, {
+    message: '비밀번호는 최소 6자 이상의 문자와 숫자의 조합이어야 합니다.',
+  })
   password: string;
 }
