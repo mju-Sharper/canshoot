75739cbe25796e416b2a2e454961ab3d1af0df62
ChoiWonYu <phillip07290@naver.com>
2023-05-01 21:26:57 +0900

    feat: 소켓 로깅

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from 'src/auth/auth.module';
+import { LoggerModule } from 'src/logger/logger.module';
 import { ProductsModule } from 'src/products/products.module';
 
 import { EventsGateway } from './events.gateway';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [AuthModule, ProductsModule, LoggerModule],
   providers: [EventsGateway],
 })
 export class EventsModule {}
