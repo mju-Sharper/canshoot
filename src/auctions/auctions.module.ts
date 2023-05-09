bd8d3245af8b05f953f1f0928c24464dbe0da65c
ChoiWonYu <phillip07290@naver.com>
2023-05-09 09:36:25 +0900

    feat: 입찰하기 api

--- src/auctions/auctions.module.ts
+++ src/auctions/auctions.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/auth/user.entity';
+
 import { Auction } from './auctions.entity';
 import { AuctionRepository } from './auctions.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auction])],
+  imports: [TypeOrmModule.forFeature([Auction, User]), AuthModule],
   providers: [AuctionRepository],
   exports: [AuctionRepository],
 })
--- src/auctions/auctions.repository.ts
+++ src/auctions/auctions.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
+import { UserRepository } from 'src/auth/user.repository';
 import { Product } from 'src/products/entities';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 
 import { Auction } from './auctions.entity';
 
@@ -10,16 +11,38 @@ import { Auction } from './auctions.entity';
 export class AuctionRepository {
   constructor(
     @InjectRepository(Auction)
-    private readonly userRepository: Repository<Auction>,
+    private readonly auctionRepository: Repository<Auction>,
+    private readonly userRepository: UserRepository,
   ) {}
 
   async createAuction(product: Product, bid: number): Promise<Auction> {
-    const auction = this.userRepository.create({
+    const auction = this.auctionRepository.create({
       product,
       bid,
     });
 
-    await this.userRepository.save(auction);
+    await this.auctionRepository.save(auction);
     return auction;
+  }
+
+  async handleBid(
+    bid: number,
+    userId: string,
+    productId: string,
+  ): Promise<UpdateResult> {
+    const user = await this.userRepository.findUserById(userId);
+
+    const updatedAuction = await this.auctionRepository
+      .createQueryBuilder('auction')
+      .leftJoinAndSelect('auction.productId', 'Products')
+      .where('auction.productId=:productId', { productId })
+      .update()
+      .set({
+        bid,
+        bidder: user,
+      })
+      .execute();
+
+    return updatedAuction;
   }
 }
